e331b5b73277942aa9cb862712e5b34632632f0a
Oleg Chausov <olegshak1985@gmail.com>
2024-10-19 17:42:50 +0300

    23 урок-fixed

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -3,19 +3,23 @@ from django.urls import reverse
 
 class PublishedModel(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(is_published="1")
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
 
 # Create your models here.
 class Women(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
     title = models.CharField(max_length=255, verbose_name="Заголовок")
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=1)#choices=Status.choices, default=Status.DRAFT)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
 
-    objects = models.Manager()
-    published = PublishedModel()
+    objects = models.Manager() #старый дефолтный менеджер менеджер, существует без объявления класса
+    published = PublishedModel() #новый менеджер
 
 
     def __str__(self):
