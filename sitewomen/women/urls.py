a4ad2bbcf6a2a8924ebd0dae2ab17e2adce51071
Oleg Chausov <olegshak1985@gmail.com>
2024-11-28 21:53:58 +0300

    made by Oleg Chausov  in footer

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path, register_converter
 from . import views
 from . import converters
+from .views import WomenCategory
 
 register_converter(converters.FourDigitYearConverter, "year4")
 
@@ -11,15 +12,18 @@ urlpatterns = [
     #path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'), #127.0.0.1:8000/cats/cat_id
     #re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive, name='archive'), #127.0.0.1:8000/archive????
     #path('archive/<year4:year>/', views.archive, name='archive'), #127.0.0.1:8000/archive???? то же с помощью конвертера 'year4'
-    path('', views.index, name='home'),
+    #path('', views.index, name='home'),
+    path('', views.WomenHome.as_view(), name='home'),#'при вызове можно переопределить переменную extra_context, которая была объявлена при объявлении класса и передать при вызове объекта класса WomenHome.as_view(extra_context={'title': "Главная страница сайта"})
     path('about/', views.about, name='about'),
-    path('addpage/', views.addpage, name='add_page'),
+    #path('addpage/', views.addpage, name='add_page'), # через функцию
+    path('addpage/', views.AddPage.as_view(), name='add_page'),# через класс
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
     path('post/<int:post_id>/', views.show_post, name='post'), # маршрут с показом статей
     path('post/<slug:post_slug>/', views.show_post, name='post'),
     #path('cats/<int:cat_id>/', views.show_category, name='category'), категории по номерам
-    path('cats/<slug:cat_slug>/', views.show_category, name='category'),
+    #path('cats/<slug:cat_slug>/', views.show_category, name='category'),
+    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
     path('tag/<slug:tag_slug>/', views.show_tag_postlist, name='tag')
 
 
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,6 +4,9 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 import uuid
 
+from django.views import View
+from django.views.generic import TemplateView, ListView
+
 from .forms import AddPostForm, UploadFileForm
 from .models import Women, Category, TagPost, UploadFiles
 
@@ -14,19 +17,50 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 ]
 
 
-def index(request):
-    # posts = Women.objects.filter(is_published=1) это со старым дефолтным менеджером
-    #posts = Women.published.all()# all вместо filter потому, что этот менеджер возвращает только те записи, которые нужны, фильтровать не нужно
-    posts = Women.published.all().select_related('cat') #select_related('cat') это метод жадной загрузки объектов из модели women, связанное с модель 'Category' поле 'cat'
+# def index(request):
+#     # posts = Women.objects.filter(is_published=1) это со старым дефолтным менеджером
+#     #posts = Women.published.all()# all вместо filter потому, что этот менеджер возвращает только те записи, которые нужны, фильтровать не нужно
+#     posts = Women.published.all().select_related('cat') #select_related('cat') это метод жадной загрузки объектов из модели women, связанное с модель 'Category' поле 'cat'
+#
+#     data = {
+#         'title': 'Главная страница',
+#         'menu': menu,
+#         'posts': posts,
+#        # 'cat_selected': 0,  # не обязательная строчка
+#     }
+#     return render(request, 'women/index.html', context=data)
+#     #return HttpResponse(render_to_string('women/index.html')) альтернативный вариаент
 
-    data = {
+class WomenHome(ListView):
+    model = Women
+    template_name = 'women/index.html' #по умолчанию вызывается шаблон <имя приложения>/<имя модели>_list.html наш шаблон другой,  то его можно так переопределить
+    context_object_name = 'posts'  #по умолчанию в шаблон передается коллекция object_list (обычно квирисет из модели), но ее можно так переопределить, переназвать
+    extra_context = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': posts,
-       # 'cat_selected': 0,  # не обязательная строчка
-    }
-    return render(request, 'women/index.html', context=data)
-    #return HttpResponse(render_to_string('women/index.html')) альтернативный вариаент
+        'cat_selected': 0,
+    }#extra_context не умеет ловить данные динамически (GET)
+
+    def get_queryset(self):
+        return Women.published.all().select_related('cat') #переопределяем выборку (queryset) для переменной context_object_name/'posts'
+
+# class WomenHome(TemplateView):
+#     template_name = 'women/index.html'
+#     # вариант логики, чтобы передать в шаблон простые данные, не подходит, чтобы ловить динамически получениые (GET) данные
+#     extra_context = {
+#          'title': 'Главная страница',
+#          'menu': menu,
+#          'posts': Women.published.all().select_related('cat'),
+#          'cat_selected': 0,
+#      }
+#
+#     # def get_context_data(self, **kwargs): # вариант логики, чтобы передать в шаблон в том числе динамически получениые (GET) данные
+#     #     context = super().get_context_data(**kwargs) #берем словарь базового класса, и дабавляем в него свои данные
+#     #     context['title'] = 'Главная страница'
+#     #     context['menu'] = menu
+#     #     context['posts'] = Women.published.all().select_related('cat')
+#     #     context['cat_selected'] = int(self.request.GET.get('cat_id', 0)) #добавляем динамически данные, которые мы ловим на лету из get запроса
+#     #     return context
 
 def show_post(request, post_slug):
     post = get_object_or_404(Women, slug=post_slug)
@@ -99,21 +133,40 @@ def page_not_found(request, exception):
     return HttpResponseNotFound(f"<h1>Страница не найдена</h1>")
 
 
-def addpage(request):
-    if request.method == 'POST':
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # try: #такая вот проверка валидности
+#             #     Women.objects.create(**form.cleaned_data)# звездочки распаковывают словарь form.cleaned_data - атрибут объекта form в виде словаря
+#             #     return redirect('home')
+#             # except:
+#             #     form.add_error(None, 'Ошибка добавления поста')
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#
+#     return render(request, 'women/addpage.html', {'menu': menu, 'title': 'Добавление статьи', 'form': form})
+
+
+class AddPage(View):
+
+    def get(self, request):
+
+        form = AddPostForm()
+        data = {'title': 'Главная страница', 'menu': menu, 'form': form}
+        return render(request, 'women/addpage.html', data)
+
+    def post(self, request):
         form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
-            # try: #такая вот проверка валидности
-            #     Women.objects.create(**form.cleaned_data)# звездочки распаковывают словарь form.cleaned_data - атрибут объекта form в виде словаря
-            #     return redirect('home')
-            # except:
-            #     form.add_error(None, 'Ошибка добавления поста')
             form.save()
             return redirect('home')
-    else:
-        form = AddPostForm()
 
-    return render(request, 'women/addpage.html', {'menu': menu, 'title': 'Добавление статьи', 'form': form})
+        data = {'title': 'Главная страница', 'menu': menu, 'form': form}
+
+        return render(request, 'women/addpage.html', data)
 
 def contact(request):
     return HttpResponse("Обратная связь")
@@ -122,18 +175,34 @@ def login(request):
     return HttpResponse("Авторизация")
 
 
-def show_category(request, cat_slug):
-    category = get_object_or_404(Category, slug=cat_slug) #ищем по модели Category, поле slug, 404 поднимается, если не найдено. Потом ту строчку (объект), что нашли возрващаем в posts = Women.published.filter(cat_id=category.pk) с атрибутом pk
-    #posts = Women.published.filter(cat_id=category.pk)
-    posts = Women.published.filter(cat_id=category.pk).select_related('cat') #select_related('cat') это метод жадной загрузки объектов из модели women, связанное с модель 'Category' поле 'cat'
-    data = {
-        'title': f'Рубрика: {category.name}',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': category.pk,
-    }
+# def show_category(request, cat_slug):
+#     category = get_object_or_404(Category, slug=cat_slug) #ищем по модели Category, поле slug, 404 поднимается, если не найдено. Потом ту строчку (объект), что нашли возрващаем в posts = Women.published.filter(cat_id=category.pk) с атрибутом pk
+#     #posts = Women.published.filter(cat_id=category.pk)
+#     posts = Women.published.filter(cat_id=category.pk).select_related('cat') #select_related('cat') это метод жадной загрузки объектов из модели women, связанное с модель 'Category' поле 'cat'
+#     data = {
+#         'title': f'Рубрика: {category.name}',
+#         'menu': menu,
+#         'posts': posts,
+#         'cat_selected': category.pk,
+#     }
+#
+#     return render(request, 'women/index.html', context=data)
 
-    return render(request, 'women/index.html', context=data)
+class WomenCategory(ListView):
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False #при пустом кверисете context_object_name = 'posts' --> (Women.published.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')) вернется 404
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cat = context['posts'][0].cat
+        context['title'] = 'Категория - ' + cat.name
+        context['menu'] = menu
+        context['cat_selected'] = cat.id
+        return context
+
+    def get_queryset(self):
+        return Women.published.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat') #self.kwargs['cat_slug'] - это переменная, определенная в соответствующем маршруте women\urls.py
 
 
 def show_tag_postlist(request, tag_slug):
